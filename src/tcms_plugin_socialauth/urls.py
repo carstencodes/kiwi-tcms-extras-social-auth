c02288d8f1189ea77d8e8616933387703b26a174
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2022-09-25 11:52:24 +0200

    Added urls according to django social auth

---
+++ src/tcms_plugin_socialauth/urls.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2022 Carsten Igel.
+#
+# This file is part of Social Auth Provider for Kiwi TCMS 
+# (see https://github.com/carstencodes/kiwi-tcms-plugins-social-auth).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from django.urls import re_path
+from django.conf.urls import include
+
+from tcms.urls import urlpatterns
+
+
+urlpatterns += [
+    re_path(r'', include('social_django.urls', namespace='social')),
+]
